8385aae225bf97eba7167fb39a9769ccbb5edb21
Anthony <Anthony@DESKTOP-K2JVO4Q.attlocal.net>
2023-01-31 18:21:05 -0600

    First Commit

---
+++ src/main/java/edu/tstc/depanfilis/depanfilis_mastery1/Depanfilis_Mastery1.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package edu.tstc.depanfilis.depanfilis_mastery1;
+
+/**
+ *
+ * @author Anthony
+ */
+public class Depanfilis_Mastery1 {
+
+    public static void main(String[] args) {
+        System.out.println( "  _      _");
+        System.out.println("  \\'\\  /'/");
+        System.out.println("   \\ \\/ /");
+        System.out.println("   / . . \\");
+        System.out.println("  =\\  T  /=");
+        System.out.println("   /  ^  \\");
+        System.out.println("{}/\\\\   //\\");
+        System.out.println(" __\\\"   \" /__    ");
+        System.out.println("(____/^\\_____)    ");
+        
+        
+    }
+}
